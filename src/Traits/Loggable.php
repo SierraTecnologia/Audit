ea1400b6fb5cfbfa1c8ee6c83d41adf6d9c2c549
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-05-12 06:55:49 -0300

    Reorganizando Controllers do Facilitador

--- src/Traits/Loggable.php
+++ src/Traits/Loggable.php
@@ -10,8 +10,8 @@ use Facilitador\Models\Image;
 use Facilitador;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Facilitador\Models\Traits\SerializeWithImages;
-use Facilitador\Models\Traits\CanSerializeTransform;
+use Support\Traits\Models\SerializeWithImages;
+use Support\Traits\Models\CanSerializeTransform;
 
 /**
  * Enable logging changes to models
